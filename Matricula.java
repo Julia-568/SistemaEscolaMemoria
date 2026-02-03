d5af4f78db433e6d67115a05c38d9c913b9a15b5
Julia Souza <juju.souza09@yahoo.com>
2026-02-03 11:36:30 -0300

    Update Matricula.java

--- Matricula.java
+++ Matricula.java
@@ -1,5 +1,22 @@
 package SistemaEscola;
 
-public class Matricula {
+import java.time.LocalDate;
 
+public class Matricula {
+	
+	private LocalDate dataMatricula;
+	private String status;
+	
+	public Matricula (LocalDate dataMatricula, String status) {
+		this.dataMatricula = dataMatricula;
+		this.status = status;
+	}
+	
+	public LocalDate getDataMatricula() {
+		return dataMatricula;
+	}
+	
+	public String getStatus() {
+		return status;
+	}
 }
