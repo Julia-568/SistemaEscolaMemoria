9cdad01291d64fa0fef777fa5dade43b4b365e18
Julia Souza <juju.souza09@yahoo.com>
2026-02-03 11:37:01 -0300

    Update Professor.java

--- Professor.java
+++ Professor.java
@@ -2,4 +2,26 @@ package SistemaEscola;
 
 public class Professor {
 
+  private String nome;
+	private String email;
+	private String especialidade;
+	
+	public Professor (String nome, String email, String especialidade) {
+		this.nome = nome;
+		this.email = email;
+		this.especialidade = especialidade;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public String getEspecialidade() {
+		return especialidade;
+	}
+
 }
