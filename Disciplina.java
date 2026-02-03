b523d81679639afbf6aa8e48dbaf2bf1daec62c7
Julia Souza <juju.souza09@yahoo.com>
2026-02-03 11:35:20 -0300

    Update Disciplina.java
    Added properties and constructor for Disciplina class

--- Disciplina.java
+++ Disciplina.java
@@ -2,4 +2,25 @@ package SistemaEscola;
 
 public class Disciplina {
 
+  private String nome;
+	private double cargaHoraria;
+	private String descricao;
+	
+	public Disciplina (String nome, double cargaHoraria, String descricao) {
+		this.nome = nome;
+		this.cargaHoraria = cargaHoraria;
+		this.descricao = descricao;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public double getCargaHoraria() {
+		return cargaHoraria;
+	}
+	
+	public String getDescricao() {
+		return
+
 }
