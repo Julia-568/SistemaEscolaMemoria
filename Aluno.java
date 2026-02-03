55f76751ec0ad56d9020e60dc48f68d096375eec
Julia Souza <juju.souza09@yahoo.com>
2026-02-03 11:33:24 -0300

    Update Aluno.java

--- Aluno.java
+++ Aluno.java
@@ -1,5 +1,31 @@
 package SistemaEscola;
 
-public class Aluno {
+import java.time.LocalDate;
 
+public class Aluno {
+	
+	private String nome;
+	private String email;
+	private LocalDate dataNascimento;
+	
+	public Aluno (String nome, String email, LocalDate dataNascimento) {
+		this.nome = nome;
+		this.email = email;
+		this.dataNascimento = dataNascimento;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
+	
+	
 }
+
