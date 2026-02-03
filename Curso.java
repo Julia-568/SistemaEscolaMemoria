558495fac679201dfc16559ea6f4fa167ad4377b
Julia Souza <juju.souza09@yahoo.com>
2026-02-03 11:34:48 -0300

    Update Curso.java

--- Curso.java
+++ Curso.java
@@ -1,5 +1,27 @@
 package SistemaEscola;
 
 public class Curso {
+  private String nome;
+	private double cargaHoraria;
+	private String descricao;
+	
+	public Curso (String nome, double cargaHoraria, String descricao) {
+		this.nome = nome;
+		this.cargaHoraria = cargaHoraria;
+		this.descricao = descricao;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public double getCargaHoraria() {
+		return cargaHoraria;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
 
 }
+
