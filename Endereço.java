8b4c03f051667815569ea28392dd0bc8932fd6dc
Julia Souza <juju.souza09@yahoo.com>
2026-02-03 11:35:54 -0300

    Update Endereço.java

--- Endereço.java
+++ Endereço.java
@@ -2,4 +2,33 @@ package SistemaEscola;
 
 public class Endereço {
 
+  private String rua;
+	private int numero;
+	private String bairro;
+	private String cidade;
+	
+	public Endereço (String rua, int numero, String bairro, String cidade) {
+		this.rua = rua;
+		this.numero = numero;
+		this.bairro = bairro;
+		this.cidade = cidade;
+	}
+	
+	public String getRua() {
+		return rua;
+	}
+	
+	public int getNumero() {
+		return numero;
+	}
+	
+	public String getBairro() {
+		return bairro;
+	}
+	
+	public String getCidade() {
+		return cidade;
+	}
+	
+
 }
