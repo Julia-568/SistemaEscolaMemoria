7d76be65ff42c26f0cdb3dab474beaa40a27d909
Julia Souza <juju.souza09@yahoo.com>
2026-02-03 11:37:35 -0300

    Update Turma.java

--- Turma.java
+++ Turma.java
@@ -2,4 +2,26 @@ package SistemaEscola;
 
 public class Turma {
 
+  private int codigoTurma;
+	private int ano;
+	private String periodo;
+	
+	public Turma (int codigoTurma, int ano, String periodo) {
+		this.codigoTurma = codigoTurma;
+		this.ano = ano;
+		this.periodo = periodo;
+	}
+	
+	public int getCodigoTurma() {
+		return codigoTurma;
+	}
+	
+	public int getAno() {
+		return ano;
+	}
+	
+	public String getPeriodo() {
+		return periodo;
+	}
+
 }
